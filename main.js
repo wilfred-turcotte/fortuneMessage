7ec14fb4fd11d2e17e52127ebda250a4fcf5e2d3
WIL <78506632+wilfred-turcotte@users.noreply.github.com>
2025-07-04 17:09:01 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+const fortunePartOne = [{ 'message1'}, { 'message2'}, { 'message3'}];
+const fortunePartTwo = [{ 'message1'}, { 'message2'}, { 'message3'}];
+const fortunePartThree = [{ 'message1'}, { 'message2'}, { 'message3'}];
